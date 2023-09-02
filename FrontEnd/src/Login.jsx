7b4dc68cb0007b082326bcfeaf7af4990a11e3d0
Tarun Jakkula <tarunjakkula1236@gmail.com>
2023-09-02 17:07:21 +0530

    login edits

--- FrontEnd/src/Login.jsx
+++ FrontEnd/src/Login.jsx
@@ -1,7 +1,23 @@
 function Login() {
   return (
     <>
-      <div className="bg-red-600">HI</div>
+      <div className="mx-[10vh] my-[5vh] max-w-full">
+        <div className="grid grid-cols-2 max-w-full h-[90vh] shadow-lg">
+          <div className="credentials-bg grid place-content-center">
+            <div
+              className="grid px-[30px] py-[60px] rounded-lg border-[2px] box-shadow-quote border-[#dfe4ff] bg-[#dfe4ff4d] backdrop-blur
+"
+            >
+              <span>Your</span>
+              <span>Destination</span>
+              <span>Awaits.</span>
+              <br />
+              <span>Unlock your educational journey</span>
+            </div>
+          </div>
+          <div>Login</div>
+        </div>
+      </div>
     </>
   );
 }
