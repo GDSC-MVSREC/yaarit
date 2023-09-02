3fcaa67de2be930335bf5a57e128a9dcba36cac5
Tarun Jakkula <tarunjakkula1236@gmail.com>
2023-09-02 17:34:49 +0530

    designed quote section

--- FrontEnd/src/Login.jsx
+++ FrontEnd/src/Login.jsx
@@ -4,15 +4,21 @@ function Login() {
       <div className="mx-[10vh] my-[5vh] max-w-full">
         <div className="grid grid-cols-2 max-w-full h-[90vh] shadow-lg">
           <div className="credentials-bg grid place-content-center">
-            <div
-              className="grid px-[30px] py-[60px] rounded-lg border-[2px] box-shadow-quote border-[#dfe4ff] bg-[#dfe4ff4d] backdrop-blur
-"
-            >
-              <span>Your</span>
-              <span>Destination</span>
-              <span>Awaits.</span>
+            <div className="px-[30px] py-[60px] rounded-2xl border-[2px] box-shadow-quote border-[#dfe4ff] bg-[#dfe4ff4d] backdrop-blur px-[70px]">
+              <p className="text-[4.2em] leading-[1.1em] font-black text-white tracking-wider">
+                Your
+              </p>
+              <p className="text-[4.2em] leading-[1.1em] font-black text-white tracking-wider">
+                Destination
+              </p>
+              <p className="text-[4.2em] leading-[1.1em] font-black text-[#15144B] tracking-wider">
+                Awaits.
+              </p>
               <br />
-              <span>Unlock your educational journey</span>
+              <br />
+              <span className="font-black text-white">
+                Unlock your educational journey
+              </span>
             </div>
           </div>
           <div>Login</div>
