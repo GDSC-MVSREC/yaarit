1f1952ab1516be61e784ce955f6457f36f12f77c
Tarun Jakkula <tarunjakkula1236@gmail.com>
2023-09-04 00:04:07 +0530

    added form tag

--- FrontEnd/src/Login.jsx
+++ FrontEnd/src/Login.jsx
@@ -1,47 +1,51 @@
+import { useEffect, useState } from "react";
 import Arrow from "./assets/Vector.svg";
 
 function Login() {
+  const [Email, setEmail] = useState("");
+  const [Password, setPassword] = useState("");
+
   return (
     <>
-      <div className="mx-[10vh] my-[5vh] max-w-full">
-        <div className="grid grid-cols-2 max-w-full h-[90vh] shadow-lg">
-          <div className="credentials-bg grid place-content-center">
-            <div className="px-[30px] py-[60px] rounded-2xl border-[2px] box-shadow-quote border-[#dfe4ff] bg-[#dfe4ff4d] backdrop-blur pl-[60px] pr-[80px] floats-vertical">
-              <div className="flex">
-                <img
-                  src={Arrow}
-                  alt=""
-                  width={"25px"}
-                  height={"45px"}
-                  className="mr-[20px]"
-                />
-                <span className="text-[4.2em] leading-[1.1em] font-black text-white tracking-wider">
-                  Your{" "}
-                </span>
-              </div>
-
-              <p className="text-[4.2em] leading-[1.1em] font-black text-white tracking-wider">
-                Destination
-              </p>
-              <p className="text-[4.2em] leading-[1.1em] font-black text-[#15144B] tracking-wider">
-                Awaits.
-              </p>
-              <br />
-              <br />
-              <span className="font-black text-white tracking-wider">
-                Unlock your educational journey
+      <div className="grid grid-cols-2 max-w-full h-screen shadow-lg">
+        <div className="credentials-bg grid place-content-center">
+          <div className="px-[30px] py-[60px] rounded-2xl border-[2px] box-shadow-quote border-[#dfe4ff] bg-[#dfe4ff4d] backdrop-blur pl-[60px] pr-[80px] floats-vertical">
+            <div className="flex">
+              <img
+                src={Arrow}
+                alt=""
+                width={"25px"}
+                height={"45px"}
+                className="mr-[20px]"
+              />
+              <span className="text-[4.2em] leading-[1.1em] font-black text-white tracking-wider">
+                Your{" "}
               </span>
             </div>
+
+            <p className="text-[4.2em] leading-[1.1em] font-black text-white tracking-wider">
+              Destination
+            </p>
+            <p className="text-[4.2em] leading-[1.1em] font-black text-[#15144B] tracking-wider">
+              Awaits.
+            </p>
+            <br />
+            <br />
+            <span className="font-black text-white tracking-wider">
+              Unlock your educational journey
+            </span>
           </div>
-          <div className="grid place-content-center">
-            <div className="grid">
-              <span className="text-[#15144B] text-[2.2em] font-black tracking-wider">
-                Hello &#128075;
-              </span>
-              <span className=" text-[#778391] text-[1.2em] font-[500] tracking-wider">
-                Enter your details
-              </span>
-              <br />
+        </div>
+        <div className="grid place-content-center">
+          <div className="grid">
+            <span className="text-[#15144B] text-[2.2em] font-black tracking-wider">
+              Hello &#128075;
+            </span>
+            <span className=" text-[#778391] text-[1.2em] font-[500] tracking-wider">
+              Enter your details
+            </span>
+            <br />
+            <form className="grid">
               <span className="text-[#15144B] text-[1.2em] font-black tracking-wider">
                 Email
               </span>
@@ -50,6 +54,11 @@ function Login() {
                 id="Email-login"
                 name="Email-login"
                 className="credentials-input"
+                value={Email}
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+                required
               ></input>
               <br />
               <span className="text-[#15144B] text-[1.2em] font-black tracking-wider">
@@ -57,9 +66,14 @@ function Login() {
               </span>
               <input
                 type="password"
-                id="Email-login"
-                name="Email-login"
+                id="Password-login"
+                name="Password-login"
                 className="credentials-input"
+                value={Password}
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+                required
               ></input>
               <br />
               <div className="flex justify-between">
@@ -71,7 +85,7 @@ function Login() {
                     value="Remember me"
                   ></input>
                   <label
-                    for="remember-me"
+                    htmlFor="remember-me"
                     className=" pl-[10px] text-[#15144B] font-black tracking-wider "
                   >
                     Remember me
@@ -82,10 +96,14 @@ function Login() {
                 </button>
               </div>
               <br />
-              <button className="text-white text-[1.5em] font-black tracking-wider credentials-button">
-                Login
-              </button>
-            </div>
+              <input
+                type="submit"
+                id="login"
+                name="login"
+                value="Login"
+                className="text-white text-[1.5em] font-black tracking-wider credentials-button"
+              ></input>
+            </form>
           </div>
         </div>
       </div>
