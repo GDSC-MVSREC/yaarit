53da806337a51e0cdeb3694d750bbd785426940e
Tarun Jakkula <tarunjakkula1236@gmail.com>
2023-09-05 14:42:32 +0530

    register page start

--- FrontEnd/src/Login/Login.jsx
+++ FrontEnd/src/Login/Login.jsx
@@ -1,11 +1,10 @@
 import { useEffect, useState } from "react";
 import Axios from "axios";
 import { useNavigate } from "react-router-dom";
-import Arrow from "../assets/Vector.svg";
 import LoginLayout from "./LoginLoayout.jsx";
 import ForgotPass from "./ForgotPass.jsx";
+import Quote from "../Quote.jsx";
 import frame from "../assets/Frame.svg";
-
 import { useMediaQuery } from "react-responsive";
 
 function Login() {
@@ -55,36 +54,7 @@ function Login() {
   return (
     <>
       <div className={`grid ${isDesktopOrLaptop ? "grid-cols-2" : ""}`}>
-        {isDesktopOrLaptop && (
-          <div className="credentials-bg grid h-screen min-h-[500px] place-content-center">
-            <div className="px-[30px] py-[60px] rounded-2xl border-[2px] box-shadow-quote border-[#dfe4ff] bg-[#dfe4ff4d] backdrop-blur pl-[60px] pr-[80px] floats-vertical">
-              <div className="flex">
-                <img
-                  src={Arrow}
-                  alt=""
-                  width={"25px"}
-                  height={"45px"}
-                  className="mr-[20px]"
-                />
-                <span className="text-[4.2em] leading-[1.1em] font-black text-white tracking-wider">
-                  Your{" "}
-                </span>
-              </div>
-
-              <p className="text-[4.2em] leading-[1.1em] font-black text-white tracking-wider">
-                Destination
-              </p>
-              <p className="text-[4.2em] leading-[1.1em] font-black text-[#15144B] tracking-wider">
-                Awaits.
-              </p>
-              <br />
-              <br />
-              <span className="font-black text-white tracking-wider">
-                Unlock your educational journey
-              </span>
-            </div>
-          </div>
-        )}
+        {isDesktopOrLaptop && <Quote />}
         <div
           className={`grid place-content-center h-screen min-h-[500px] relative`}
         >
